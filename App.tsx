bbdf06d9c53342b3283fbf2b24a884a5b882d5dd
dallinhumphrey <dallinhumphrey@gmail.com>
2020-12-15 15:04:38 -0700

    adding feed

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+
+import FeedScreen from "./screens/FeedScreen";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FeedScreen />
     </View>
   );
 }
@@ -14,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'steelblue',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ screens/FeedScreen.tsx
@@ -0,0 +1,11 @@
+import * as expoStatusBar from 'expo-status-bar';
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default () => {
+  return (
+     <View>
+      <Text>Feed the pigs</Text>
+    </View>
+  );
+};
