304529e3890e647fdc980e7778e729ca07493633
dallinhumphrey <dallinhumphrey@gmail.com>
2020-12-21 11:01:53 -0700

    Typescript interfaces

--- screens/FeedScreen.tsx
+++ screens/FeedScreen.tsx
@@ -2,7 +2,12 @@ import * as expoStatusBar from 'expo-status-bar';
 import React from "react";
 import { Text, View, TouchableOpacity } from "react-native";
 
-export default props => {
+interface IFeedScreenProps{
+  navigation: {
+    navigate: (arg: string) => void;
+  }
+}
+export default (props: IFeedScreenProps) => {
   return (
     <View>
       <Text>Feed screen</Text>
