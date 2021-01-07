64a626852d69c0ac660e50c69bdc1b36e822427e
dallinhumphrey <dallinhumphrey@gmail.com>
2021-01-07 14:29:56 -0700

    added bottom tab styles

---
+++ styles/navigation/bottomTabStyles.ts
@@ -0,0 +1,9 @@
+import {StyleSheet} from "react-native";
+
+export default StyleSheet.create({
+    container: {
+        backgroundColor: "steelblue",
+        paddingTop: 15,
+        paddingBottom: 15
+    }
+});
