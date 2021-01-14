cd92c3f34ee34b64f5ee23f117398dcd3441e775
dallinhumphrey <dallinhumphrey@gmail.com>
2021-01-14 14:22:59 -0700

    react native style options

--- utils/router.tsx
+++ utils/router.tsx
@@ -7,15 +7,24 @@ import SearchScreen from "../screens/SearchScreen";
 import AccountScreen from "../screens/AccountScreen";
 import PostFormScreen from "../screens/PostFormScreen";
 
+import {dark} from "../styles/colors";
+
 const AppStack = createStackNavigator(
   {
+
     Feed: FeedScreen,
     Search: SearchScreen,
     Account: AccountScreen,
     PostForm: PostFormScreen
   },
   {
-    initialRouteName: "Feed"
+    initialRouteName: "Feed",
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: dark
+      },
+      headerTintColor: "#fff"
+    } 
   }
 );
 
