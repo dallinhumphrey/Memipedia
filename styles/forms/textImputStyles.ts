c0809adb977e9c1b31b9effc3d13cb03e83538fb
dallinhumphrey <dallinhumphrey@gmail.com>
2021-01-23 14:09:10 -0700

    stylesheet part 1

---
+++ styles/forms/textImputStyles.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+import { dark } from "../colors";
+
+export default StyleSheet.create({
+    textFieldWrapper: {
+        marginTop: 100, 
+        marginBottom: 20
+    },
+    textField: {
+            backgroundColor: "white",
+            borderRadius: 20,
+            height: 40,
+            paddingLeft: 20
+        }
+});
